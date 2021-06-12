f419f96fc78386715fa53ecdd5d3f6d65438295d
anandhumanoj <anandhumanoj282@gmail.com>
2021-06-12 12:58:35 +0000

    fix: matrix integration
    I must be the dumbest person on the planet, totally misunderstood matrix-js-sdk. Never needed to call startClient() method at all

--- integrations/matrix.js
+++ integrations/matrix.js
@@ -2,28 +2,17 @@ import sdk from 'matrix-js-sdk'
 
 var client = null;
 
-const withConnection = (callback, ...args) => {
+const withConnection = async (callback, ...args) => {
     client = sdk.createClient({
         baseUrl: "https://matrix.org",
         accessToken: process.env.MATRIX_ACCESS_TOKEN,
         userId: process.env.MATRIX_USER_ID
     });
-    console.log('Starting matrix client');
-    client.startClient();
-    console.log('Starting matrix /sync call');
-    client.once('sync', (state, prevState, res) => {
-        console.log("Matrix client sync state updated. New state:", state); 
-        console.log("Callback is: ", callback, args);
-        if (state == "PREPARED" && typeof callback == 'function'){
-            console.log("Executing callback: ", callback, args);
-            callback(...args);
-            client.close()
-        }
-    });
-    console.log("withConnection end");
+    console.log('Executing ', callback, args);
+    await callback(...args);
 }
 
-const sendNotificationImpl = (message) => {
+const sendNotificationImpl = async (message) => {
     var roomId = process.env.MATRIX_TARGET_ROOM_ID;
 
     const notificationContent = {
@@ -31,7 +20,7 @@ const sendNotificationImpl = (message) => {
         "msgtype": "m.text"
     }
 
-    client.sendEvent(roomId, "m.room.message", notificationContent, "").then((res) => {
+    await client.sendEvent(roomId, "m.room.message", notificationContent, "").then((res) => {
         console.log("Notification sent successfully. Here is the response from matrix:", res);
     }).catch((err) => {
         console.log("Couldn't sent notification, error:", err);

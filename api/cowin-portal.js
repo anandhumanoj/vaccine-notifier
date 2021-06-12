9d89552741fede888064273ede99696e8c897f62
anandhumanoj <anandhumanoj282@gmail.com>
2021-06-12 03:19:06 +0000

    feat: basic query

--- api/cowin-portal.js
+++ api/cowin-portal.js
@@ -1,7 +1,17 @@
 module.exports = (req, res) => {
+  
+  var response = await fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=685515&date=12-06-2021", {
+    "credentials": "omit",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.5"
+    },
+    "referrer": "https://www.cowin.gov.in/",
+    "method": "GET",
+    "mode": "cors"
+  });
   res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
+    response:response
   })
 }

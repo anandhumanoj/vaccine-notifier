2234fe12eaa3d642df2e6ac21143c47ac275ddb1
anandhumanoj <anandhumanoj282@gmail.com>
2021-06-12 04:37:31 +0000

    feat: implement structured responses

--- api/cowin-portal.js
+++ api/cowin-portal.js
@@ -1,8 +1,48 @@
 import fetch from 'node-fetch'
 
+const getCurrentDate = () =>{
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1;
+
+  var yyyy = today.getFullYear();
+  if (dd < 10) {
+      dd = '0' + dd;
+  }
+  if (mm < 10) {
+      mm = '0' + mm;
+  }
+  return dd + '-' + mm + '-' + yyyy;
+
+}
+
+const parseAPIResponse = (response) => {
+
+  if(Array.isArray(response.centers) && response.centers.length === 1){
+    const center = response.centers[0];
+    if(Array.isArray(center.sessions) && center.sessions.length === 1){
+      const session = center.sessions[0];
+      return session;
+    }
+  }
+};
+
+const generateAPIResult = (data) => {
+  return {
+    available: data.available_capacity,
+    min_age: data.min_age,
+    vaccine: data.vaccine,
+    date: data.date
+  }
+}
+
 module.exports = (req, res) => {
+  const pincode = process.env.PIN_CODE;
+  const today = getCurrentDate();
+
+  const URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${today}`
   
-  fetch("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=685515&date=12-06-2021", {
+  fetch(URL, {
     "credentials": "omit",
     "headers": {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
@@ -14,10 +54,9 @@ module.exports = (req, res) => {
     "mode": "cors"
   }).then(response => response.json())
   .then(data =>{
-    res.json({
-      response:data
-    })
+    res.json(generateAPIResult(parseAPIResponse(data)))
   }).catch(error =>{
     console.error(error);
+    res.status(500).json({status: 500, message: "Internal Server Error"});
   });
 }

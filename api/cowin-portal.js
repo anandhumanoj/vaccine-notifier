92ae5391012d731b983baf528e62278d6a01a5a9
anandhumanoj <anandhumanoj282@gmail.com>
2021-06-13 15:40:39 +0000

    feat(cowin-integration): allow multiple monitor locations

--- api/cowin-portal.js
+++ api/cowin-portal.js
@@ -1,52 +1,59 @@
 import { fetchFromCowinAPI } from '../integrations/cowin'
 import { sendNotification } from '../integrations/matrix';
-import {getErrorJSON} from '../lib/api'
+import { getErrorJSON } from '../lib/api'
 
 const debug = !!process.env.ENABLE_DEBUG;
 
 const buildSuccessNotificationMessage = (cowinResponse) => {
-  var message = 
-`Hey You!,
-${cowinResponse.vaccine} is available at the configured facility. 
+  var message =
+    `Hey You!,
+${cowinResponse.vaccine} is available at ${cowinResponse.name}, ${cowinResponse.address}. 
   Number of slot available: ${cowinResponse.available}
+  Dose 1: ${cowinResponse.available_dose1}
+  Dose 2: ${cowinResponse.available_dose2}
   Date: ${cowinResponse.date}
-  Minimum age limit: ${cowinResponse.min_age}
+  Minimum age limit: ${cowinResponse.min_age_limit}
 Book now! ✨✨✨`;
   return message;
 }
 
 const buildInvokeNotificationMessage = (cowinResponse) => {
-  return "No slots available. Executed at: " + new Date();
+  return "No slots available at monitored locations.";
 }
 
 const buildFailureNotificationMesssage = (error) => {
-  return "Error occured during execution: error:" + JSON.stringify(error);  
+  return "Error occured during execution.\r\n Cause:" + JSON.stringify(error);
 }
 
 const invokeDebugNotifier = (res, message) => {
-  if(debug){
-    return sendNotification(message).catch(_ => new Promise().resolve());
+  if (debug) {
+    return sendNotification(message).catch(_ => new Promise(() => null).resolve());
   }
-  return new Promise().resolve();
+  return new Promise(() => null).resolve();
 }
 
 module.exports = (req, res) => {
-  fetchFromCowinAPI().then(response =>{
-    if(response.available > 0){
-      sendNotification(buildSuccessNotificationMessage(response)).then(resp => {
+  fetchFromCowinAPI().then(async centers => {
+    let anyZoneAvailable = false;
+    for (let center in centers) {
+      if (centers[center].available > 0) {
+        anyZoneAvailable = true;
+        await sendNotification(buildSuccessNotificationMessage(centers[center]));
+      }
+    }
+    if (anyZoneAvailable) {
+      res.status(200).send({
+        status: 200,
+        message: "Hurry! free allotment found, Book Now!",
+      });
+    } else {
+      invokeDebugNotifier(res, buildInvokeNotificationMessage(centers)).then(_ => {
         res.status(200).send({
           status: 200,
-          message: "Hurry! free allotment found, Book Now!",
-          raw_response: response
+          message: "No slot available this time"
         });
       });
     }
-    invokeDebugNotifier(res, buildInvokeNotificationMessage(response)).then(_ => {
-      res.status(200).send({
-        status: 200,
-        message: "No slot available this time"
-      });
-    });
   }).catch(error => {
     console.error(error);
     invokeDebugNotifier(res, buildFailureNotificationMesssage(error)).then(_ => {
--- integrations/cowin.js
+++ integrations/cowin.js
@@ -4,6 +4,16 @@ import UserAgent from 'user-agents';
 import { getErrorJSON } from "../lib/api";
 
 const debug = !!process.env.ENABLE_DEBUG;
+const PIN_CODES = (process.env.PIN_CODES || process.env.PIN_CODE || "").split(',').map(val => val.trim());
+const MIN_AGE_LIMITS = (process.env.MIN_AGE__LIMITS || "18,40,45").split(',').map(val => Number.parseInt(val.trim()));
+
+// let DEFAULT_URL_TYPE = Math.floor(Math.random() * URL_TYPE.length)
+let DEFAULT_URL_TYPE = 1; // DISTRICT only mode
+
+const URL_TYPES = [
+    'PIN_CODE',
+    'DISTRICT'
+]
 
 const getCurrentDate = () => {
     var today = new Date();
@@ -22,59 +32,88 @@ const getCurrentDate = () => {
 }
 
 const parseAPIResponse = (response) => {
+    var matched_rows = {};
+    if (Array.isArray(response.centers)) {
+        response.centers.forEach((center) => {
+            if (PIN_CODES.includes(center.pincode.toString())) {
+                if (Array.isArray(center.sessions)) {
+                    center.sessions.forEach( session => {
+                        if (MIN_AGE_LIMITS.includes(session.min_age_limit)) {
+                            var matched_row = {}
+                            matched_row.name = center.name;
+                            matched_row.pincode = center.pincode;
+                            matched_row.address = center.address;
+                            matched_row.date = center.date;
+                            
+                            matched_row.available = session.available_capacity;
+                            matched_row.available_dose1 = session.available_capacity_dose1;
+                            matched_row.available_dose2 = session.available_capacity_dose2;
+                            matched_row.min_age_limit = session.min_age_limit;
+                            matched_row.vaccine = session.vaccine;
 
-    if (Array.isArray(response.centers) && response.centers.length === 1) {
-        const center = response.centers[0];
-        if (Array.isArray(center.sessions) && center.sessions.length === 1) {
-            const session = center.sessions[0];
-            return session;
-        }
+                            matched_rows[center.pincode + '__' + session.min_age_limit] = matched_row;
+                        }
+                    });
+                }
+            }
+        });
+        return matched_rows;
     }
-    return {
-        __parse_failure: true
-    }
+    return getErrorJSON();
 };
 
-const generateAPIResult = (data) => {
-    if (data.__parse_failure) {
-        return getErrorJSON();
+
+const generateAPIUrl = () => {
+    
+    let URL = "";
+    const today = getCurrentDate();
+
+    switch (URL_TYPES[DEFAULT_URL_TYPE]) {
+        case 'PIN_CODE':
+            const pincode = process.env.PIN_CODE;
+            URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${today}`;
+            break;
+        case 'DISTRICT':
+            const district_id = process.env.DISTRICT_ID
+            URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district_id}&date=${today}`;
+            break;
     }
+    return [URL, URL_TYPES[DEFAULT_URL_TYPE]];
+}
+
+const constructURL = () => {
+    const randomUserAgent = new UserAgent().random().toString();
+    const [url, urlType] = generateAPIUrl();
     return {
-        available: data.available_capacity,
-        min_age: data.min_age_limit,
-        vaccine: data.vaccine,
-        date: data.date
+        url: url,
+        type: urlType,
+        opts: {
+            "credentials": "omit",
+            "headers": {
+                "User-Agent": randomUserAgent,
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "en-US,en;q=0.5",
+            },
+            "referrer": "https://www.cowin.gov.in/",
+            "method": "GET",
+            "mode": "cors"
+        }
     }
 }
 
 const fetchFromCowinAPI = () => {
-    const pincode = process.env.PIN_CODE;
-    const today = getCurrentDate();
-
-    const URL = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${today}`
+    const urlSpec = constructURL();
     if (debug) {
-        console.log(`Constructed CoWin URL: ${URL}`);
+        console.log(`Constructed CoWin URL: ${urlSpec.url}`);
     }
-    const randomUserAgent = new UserAgent().random().toString();
-    console.log(randomUserAgent);
-    return fetch(URL, {
-        "credentials": "omit",
-        "headers": {
-            "User-Agent": randomUserAgent,
-            "Accept": "application/json, text/plain, */*",
-            "Accept-Language": "en-US,en;q=0.5",
-        },
-        "referrer": "https://www.cowin.gov.in/",
-        "method": "GET",
-        "mode": "cors"
-    })
+    return fetch(urlSpec.url, urlSpec.opts)
         .then(response => response.json())
         .then(data => {
             if (debug) {
                 console.log("API response from upstream CoWin API", data);
             }
             return new Promise((resolve, reject) => {
-                var result = generateAPIResult(parseAPIResponse(data));
+                var result = parseAPIResponse(data);
                 if (debug) {
                     console.log("Restructured response: ", result);
                 }
@@ -83,8 +122,6 @@ const fetchFromCowinAPI = () => {
                 }
                 resolve(result);
             });
-        }).catch(error => {
-            throw error;
         });
 }
 

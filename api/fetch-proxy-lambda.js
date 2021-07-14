6e4e4d2db77f4117cc3e909b1455a42666304836
Anandhu Manoj <38727945+anandhumanoj@users.noreply.github.com>
2021-07-14 18:20:27 +0530

    chore: log request body

--- api/fetch-proxy-lambda.js
+++ api/fetch-proxy-lambda.js
@@ -1,6 +1,7 @@
 import fetch from 'node-fetch'
 
 module.exports = async (req,res) => {
+    console.log(req.body);
     if(req.body){
         var response = await fetch(req.body.url, req.body.opts).then(response => response.json());
         res.status(200).send(response)

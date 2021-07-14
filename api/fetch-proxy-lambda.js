243360624a9817cdbedebadce13b337b4b3d0ac6
Anandhu Manoj <38727945+anandhumanoj@users.noreply.github.com>
2021-07-14 18:33:03 +0530

    fix: disable json parsing

--- api/fetch-proxy-lambda.js
+++ api/fetch-proxy-lambda.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 module.exports = async (req,res) => {
     console.log(req.body);
     if(req.body){
-        var response = await fetch(req.body.url, req.body.opts).then(response => response.json());
+        var response = await fetch(req.body.url, req.body.opts);
         res.status(200).send(response)
     } else {
         res.status(500).error("Body missing");

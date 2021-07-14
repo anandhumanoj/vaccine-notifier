2ec385373e0074dacfdc61bfca043d12cc7bb394
Anandhu Manoj <38727945+anandhumanoj@users.noreply.github.com>
2021-07-14 18:08:54 +0530

    fix: fetch opts and urls

--- api/fetch-proxy-lambda.js
+++ api/fetch-proxy-lambda.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 module.exports = async (req,res) => {
     if(req.body){
-        var response = await fetch(req.body).then(response => response.json());
+        var response = await fetch(req.body.url, req.body.opts).then(response => response.json());
         res.status(200).send(response)
     } else {
         res.status(500).error("Body missing");
